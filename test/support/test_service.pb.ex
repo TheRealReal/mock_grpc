2b55ff6af8da03f4259dc7fa983b2866011f2032
Guilherme Sehn <guilherme.sehn@therealreal.com>
2023-06-19 19:15:12 -0300

    mix format

--- test/support/test_service.pb.ex
+++ test/support/test_service.pb.ex
@@ -2,15 +2,15 @@ defmodule TestSupport.HelloWorldRequest do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :first_name, 1, type: :string, json_name: "firstName"
-  field :last_name, 2, type: :string, json_name: "lastName"
+  field(:first_name, 1, type: :string, json_name: "firstName")
+  field(:last_name, 2, type: :string, json_name: "lastName")
 end
 
 defmodule TestSupport.HelloWorldResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :message, 1, type: :string
+  field(:message, 1, type: :string)
 end
 
 defmodule TestSupport.TestService do
@@ -19,7 +19,7 @@ defmodule TestSupport.TestService do
     name: "test_support.TestService",
     protoc_gen_elixir_version: "0.11.0"
 
-  rpc :HelloWorld, TestSupport.HelloWorldRequest, TestSupport.HelloWorldResponse
+  rpc(:HelloWorld, TestSupport.HelloWorldRequest, TestSupport.HelloWorldResponse)
 end
 
 defmodule TestSupport.TestService.Stub do
