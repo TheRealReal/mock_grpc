290fcea3961c8c49545699e78a6bb7cf7d479a94
Guilherme Sehn <ghsehn@gmail.com>
2023-06-19 16:33:49 -0300

    Automatic support for `Task.async/1` on async tests (#7)

--- lib/mock_grpc/util.ex
+++ lib/mock_grpc/util.ex
@@ -19,4 +19,27 @@ defmodule MockGRPC.Util do
       parts |> Enum.drop(-1) |> Module.concat()
     end
   end
+
+  # Get test key from the current process dictionary, or from any of the
+  # $callers process dictinary, so that it works with `Task`.
+  # More info: https://hexdocs.pm/elixir/1.15.0/Task.html#module-ancestor-and-caller-tracking
+  def get_test_key do
+    Process.get(MockGRPC) || get_test_key_from_callers() || :global
+  end
+
+  defp get_test_key_from_callers do
+    callers = Process.get(:"$callers", [])
+    get_test_key_from_callers(callers)
+  end
+
+  defp get_test_key_from_callers([]), do: nil
+
+  defp get_test_key_from_callers([caller | rest]) do
+    test_key =
+      if info = Process.info(caller) do
+        info[:dictionary][MockGRPC]
+      end
+
+    test_key || get_test_key_from_callers(rest)
+  end
 end
