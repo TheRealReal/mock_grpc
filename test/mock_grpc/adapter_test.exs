53233d23fae970979bdedc78e665c9d69e01d169
Guilherme Sehn <guilherme.sehn@therealreal.com>
2025-06-26 14:43:11 -0300

    rename test cases

--- test/mock_grpc/adapter_test.exs
+++ test/mock_grpc/adapter_test.exs
@@ -2,14 +2,14 @@ defmodule MockGRPC.AdapterTest do
   use ExUnit.Case
 
   describe "send_request/3" do
-    test "works with binary" do
+    test "works with binary (elixir-grpc < 0.10.0)" do
       stream = %GRPC.Client.Stream{request_mod: TestSupport.SayHelloRequest}
       struct = %TestSupport.SayHelloRequest{first_name: "John", last_name: "Doe"}
       content = Protobuf.Encoder.encode(struct)
       assert MockGRPC.Adapter.send_request(stream, content, []).payload.input == struct
     end
 
-    test "works with iodata" do
+    test "works with iodata (elixir-grpc >= 0.10.0)" do
       stream = %GRPC.Client.Stream{request_mod: TestSupport.SayHelloRequest}
       struct = %TestSupport.SayHelloRequest{first_name: "John", last_name: "Doe"}
       content = Protobuf.Encoder.encode_to_iodata(struct)
